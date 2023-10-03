6c1ebb68fe9f11302cae86ed68ed455412d3859f
Begginer617 <skorupskimilosz@gmail.com>
2023-10-04 00:12:55 +0200

    TRy and catch 1 komit

---
+++ src/main/java/Exceptions/Exceptions.java
@@ -0,0 +1,27 @@
+package Exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+        System.out.println("Podaj pierwsza liczbe");
+        int firstNumber;
+        boolean flag = true;
+
+
+        while (flag) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                firstNumber = scanner.nextInt();
+                flag = false;
+            } catch (InputMismatchException error1) {
+                System.out.println("Tylko liczby sa przyjmowane");
+            }
+        }
+    }
+}
+
+
+
